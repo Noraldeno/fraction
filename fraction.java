7ca169aaf83246e937f473ec517bc0c98b7a6959
Norald Alejo <norald.alejo@gmail.com>
2016-08-27 14:57:21 -0700

    First commit

---
+++ fraction.java
@@ -0,0 +1,25 @@
+/*Name: Norald Alejo
+Email: norald.alejo@gmail.com
+github: https://github.com/Noraldeno/fraction.git
+*/
+
+import java.util.Scanner
+
+public class fraction{
+	private int numerator;
+	private int denominator;
+
+	public fraction(){
+		numerator = 0;
+		denominator = 1;
+	}
+	public fraction(int num, int den){
+		numerator = num;
+		denominator = den;
+	}
+
+	public 
+	
+	public static void main (String [] args){
+
+
